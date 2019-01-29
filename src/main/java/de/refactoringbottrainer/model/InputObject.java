7566423aaa68ac56c7660cd7276d41ed4a138b35
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2019-01-29 13:20:00 +0100

    Improved anonymization, made API to PostMethod

---
+++ src/main/java/de/refactoringbottrainer/model/InputObject.java
@@ -0,0 +1,15 @@
+package de.refactoringbottrainer.model;
+
+public class InputObject {
+
+	String comment;
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+}
--- src/main/java/de/refactoringbottrainer/rest/BotTrainingController.java
+++ src/main/java/de/refactoringbottrainer/rest/BotTrainingController.java
@@ -3,13 +3,14 @@ package de.refactoringbottrainer.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
 
 import de.refactoringbottrainer.api.WitDataGrabber;
+import de.refactoringbottrainer.model.InputObject;
 import de.refactoringbottrainer.model.WitObject;
 import io.swagger.annotations.ApiOperation;
 
@@ -22,12 +23,12 @@ public class BotTrainingController {
 	@Autowired
 	DataAnonymizer anonymizer;
 
-	@GetMapping(value = "/getObjectFromMessage/{message}", produces = "application/json")
+	@PostMapping(value = "/getObjectFromMessage/{message}", produces = "application/json")
 	@ApiOperation(value = "Get Wit-Object from a message.")
-	public ResponseEntity<?> getWitObject(@PathVariable String message) {
+	public ResponseEntity<?> getWitObject(@RequestBody InputObject message) {
 		try {
-			message = anonymizer.anonymizeComment(message);
-			return new ResponseEntity<WitObject>(dataGrabber.getWitObjectFromMessage(message), HttpStatus.OK);
+			String anonymizedComment = anonymizer.anonymizeComment(message.getComment());
+			return new ResponseEntity<WitObject>(dataGrabber.getWitObjectFromMessage(anonymizedComment), HttpStatus.OK);
 		} catch (RestClientException e) {
 			return new ResponseEntity<String>("Could not connect with Wit-API!", HttpStatus.INTERNAL_SERVER_ERROR);
 		}
